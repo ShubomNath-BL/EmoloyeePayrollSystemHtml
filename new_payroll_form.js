ba0daf78e8147b732f4d67c7ddd3d570ff311dea
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-08 22:12:15 +0530

    Added: Data added to the local storage

--- new_payroll_form.js
+++ new_payroll_form.js
@@ -23,9 +23,21 @@ name.addEventListener("input",function(){
 const save = () => {
     try{
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     }catch (e){
         return;
     }
+}
+
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(employeePayrollData);
+    }else{
+        employeePayrollList = [employeePayrollData]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
 }
 
 const createEmployeePayroll = () => {
